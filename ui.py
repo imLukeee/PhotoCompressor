04fbf6188a957258d636a711c5d51045f2891d4d
Lukasz Jobczynski <lukaszjobczynski@gmail.com>
2025-08-10 19:03:07 +0200

    Added file size reduction output and centered image in preview

--- ui.py
+++ ui.py
@@ -2,6 +2,7 @@ import customtkinter as ctk
 from PIL import Image, ImageTk
 from tkinter import filedialog, messagebox
 from settings import *
+import os
 
 
 class Initiate_App(ctk.CTkButton):
@@ -15,11 +16,16 @@ class Initiate_App(ctk.CTkButton):
         if not filepath:
             return
         
+        #get original size in kilobytes
+        self.original_size = os.stat(filepath).st_size / 1000
+        
+        #once image is imported, delete the import button
         self.original_image = Image.open(filepath)
         self.place_forget()
 
+        #display image preview and user controls
         self.image_preview = ImageCanvas(self.main_window, self.original_image)
-        self.controls = UserControls(self.main_window, self.original_image)
+        self.controls = UserControls(self.main_window, self.original_image, self.original_size)
 
 
 class ImageCanvas(ctk.CTkCanvas):
@@ -46,20 +52,24 @@ class ImageCanvas(ctk.CTkCanvas):
             self.image_width = event.width
             self.image_height = int(self.image_width / image_ratio)
         
-        self.place_image()
+        self.place_image(event)
 
-    def place_image(self):
+    def place_image(self, event):
         resized_pil = self.original_pil.resize((self.image_width, self.image_height))
         self.image_tk = ImageTk.PhotoImage(resized_pil)
 
+        self.center_width = (event.width - self.image_width) / 2
+        self.center_height = (event.height - self.image_height) / 2
+
         self.delete('all')
-        self.create_image(0,0, anchor = 'nw', image = self.image_tk)
+        self.create_image(self.center_width, self.center_height, anchor = 'nw', image = self.image_tk)
 
 
 class UserControls(ctk.CTkFrame):
-    def __init__(self, parent, original):
+    def __init__(self, parent, original, original_size):
         super().__init__(parent, fg_color= ACCENT_COLOR)
         self.original_image = original
+        self.orignal_size = original_size
         self.place(relx = 0.05, rely = 0.6, relwidth = 0.9, relheight = 0.35, anchor = 'nw')
         self.filename = ctk.StringVar(self, value = '')
 
@@ -75,7 +85,14 @@ class UserControls(ctk.CTkFrame):
     
     def compress_image(self):
         name = self.filename.get()
+        filename = f'Compressed_{name}.jpeg'
         self.original_image = self.original_image.convert('RGB')
-        self.original_image.save(f"Compressed_{name}.jpeg", format = 'jpeg', optimize = True, quality = 10)
+        self.original_image.save(filename, format = 'jpeg', optimize = True, quality = 10)
 
-        messagebox.showinfo('Image compressed', 'Image compressed \nSize reduced by: 2137' )
+        #get compressed image size in kilobytes
+        self.compressed_size = os.stat(filename).st_size / 1000
+
+        reduced_size = round(self.orignal_size - self.compressed_size, 2)
+        percentage = round((self.orignal_size - self.compressed_size)/self.orignal_size * 100, 2)
+
+        messagebox.showinfo('Image compressed', f'Image compressed \nSize reduced by {percentage}% ({reduced_size} kB) 🤩' )
